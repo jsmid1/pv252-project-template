8281aa6cfedb4722024191050dee247eb0f22c77
jsmid1 <jsmid@redhat.com>
2024-09-23 13:46:33 +0200

    Improves test coverage

--- src/fibonacci.test.ts
+++ src/fibonacci.test.ts
@@ -3,3 +3,10 @@ import { fibonacci } from "./fibonacci.ts";
 test("fibonacci-5", () => {
   expect(fibonacci(5)).toBe(5);
 });
+
+test("fibonacci-minus", () => {
+  const will_throw = () => {
+    fibonacci(-1);
+  };
+  expect(will_throw).toThrow("Cannot compute on negative numbers");
+});
