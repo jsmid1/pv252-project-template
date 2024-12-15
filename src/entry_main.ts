c26e7c15d00534f180d53a2ed7912578a76ce828
jsmid1 <jsmid@redhat.com>
2024-12-15 22:03:40 +0100

    [Websockets] Implements drawing

--- src/entry_main.ts
+++ src/entry_main.ts
@@ -30,25 +30,55 @@ interface UpdateMessage {
   value: boolean,
 }
 
+let messageCounter = 0;
+
 // Create a websocket connection. 
 // More info at https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API
 const socket = new WebSocket("ws:socket.zavazadlo.unsigned-short.com");
 socket.onmessage = (m) => {
   console.log(m)
+
+  if (messageCounter == 0) {
+    const welcomeMessage: WelcomeMessage = JSON.parse(m.data);
+
+    canvas.width = welcomeMessage.x;
+    canvas.height = welcomeMessage.y;
+
+    const points = welcomeMessage.data;
+
+    for (let x = 0; x < canvas.width; x++) {
+      for (let y = 0; y < canvas.height; y++) {
+        const value: boolean = points[x * canvas.width + y] == 1;
+        canvas.setPixel(x, y, value)
+      }
+    }
+  } else {
+    const updates: UpdateMessage[] = JSON.parse(m.data);
+    updates.forEach(function (update: UpdateMessage) {
+      canvas.setPixel(update.point.x, update.point.y, update.value);
+    });
+  }
+
+  messageCounter++;
 }
 
 // Example of how to use the canvas element:
-let canvas = new SocketCanvasElement();
+const canvas = new SocketCanvasElement();
 canvas.width = 128;
 canvas.height = 128;
-canvas.ondraw = (x,y) => {
-  console.log(x,y);
+canvas.ondraw = (x, y) => {
+  console.log(x, y);
+
+  const point: Point = { x: x, y: y }
+  const message: UpdateMessage = { point: point, value: true };
+  socket.send(JSON.stringify(message));
+
 }
 document.querySelector("#container")!.appendChild(canvas);
 
 // We can only draw into canvas once it is actually shown, hence we postpose the draw operation.
 setTimeout(() => {
-  for (let x=0; x<128; x++) {
-    canvas.setPixel(x,x,true);
+  for (let x = 0; x < 128; x++) {
+    canvas.setPixel(x, x, true);
   }
 })
